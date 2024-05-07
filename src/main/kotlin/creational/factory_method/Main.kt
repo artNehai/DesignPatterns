7397a34380a571f460d3e9255623f34d7d5eb7c5
artNehay <artemynehay@gmail.com>
2024-05-07 19:22:58 +0300

    Implement the factory method example code

---
+++ src/main/kotlin/creational/factory_method/Main.kt
@@ -0,0 +1,27 @@
+package creational.factory_method
+
+import util.trace
+
+fun main() {
+    val localCFO = "CFO Berlin"
+    val factory: BMWFactory =
+        when (localCFO) {
+            "CFO Berlin" -> BMWBerlinDieselFactory()
+            "CFO Frankfurt" -> BMWFrankfurtGasolineFactory()
+            else -> {
+                println("New factory is being built")
+                return
+            }
+        }
+
+    // Today work done by the factory
+    trace(factory.deliverEngine())
+    trace(factory.tuneEngine())
+    trace(factory.deliverEngine())
+
+    trace eq """
+        You forgot to tune the engine!
+        Engine is tuned. Works with diesel
+        Successfully delivered
+    """
+}

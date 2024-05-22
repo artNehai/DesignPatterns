373b482c0104954cc190186515cb1d495570626f
artNehay <artemynehay@gmail.com>
2024-05-22 16:01:59 +0300

    Rename each Main.kt file with the appropriate pattern name

--- src/main/kotlin/structural/adapter/Adapter.kt
+++ src/main/kotlin/structural/adapter/Adapter.kt
@@ -1,5 +1,7 @@
 package structural.adapter
 
+import util.trace
+
 interface EuroPlug {
     val poles: String
 }
@@ -14,4 +16,24 @@ class EuroPlugAdapter : EuroPlug {
     override val poles = americanPlug.prongs
         .replace(oldValue = "flat", newValue = "round")
         .replace(oldValue = "prongs", newValue = "poles")
+}
+
+class EuroSocket(
+    private val euroPlug: EuroPlug,
+) {
+    fun connect() = """
+        My plug has ${euroPlug.poles}.
+        Fortunately, I didn't forget the adapter!
+    """.trimIndent()
+}
+
+fun main() {
+    val myEuroPlugAdapter = EuroPlugAdapter()
+    val euroSocket = EuroSocket(euroPlug = myEuroPlugAdapter)
+    trace(euroSocket.connect())
+
+    trace eq """
+        My plug has two round poles.
+        Fortunately, I didn't forget the adapter!
+    """
 }
